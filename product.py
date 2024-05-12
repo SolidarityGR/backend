bf480269e61cee1a3e68af9fa30c5ff509fb13af
laskas1234 <gbalaskas302@gmail.com>
2024-05-12 15:30:07 +0300

    product page

---
+++ product.py
@@ -0,0 +1,25 @@
+from uuid import uuid4
+
+class Product:
+    def __init__(self,
+                 product_name:str,
+                 quantity: int,
+                 description: str,
+                 photo: str,
+                 location: str,
+                 category: str,
+                 user_rank: float,
+                 username: str,
+                 user_id: uuid4,                 
+                 ) -> None:
+        
+        self.name = product_name
+        self.price = quantity
+        self.quantity = quantity
+        self.description = description
+        self.photo = photo
+        self.location = location
+        self.category = category
+        self.user_rank = user_rank
+        self.username = username
+        self.user_id = user_id
