3fa6c73ba1a5edd935e1020c8a34675e2d3f449e
laskas1234 <gbalaskas302@gmail.com>
2024-05-12 17:46:44 +0300

    at product entry error

--- product.py
+++ product.py
@@ -6,64 +6,125 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.dialects.sqlite import BLOB
 from sqlalchemy.exc import SQLAlchemyError
 from pathlib import Path
+import re, json
 
 Base = declarative_base()
+
+parent_path = Path().resolve()
+db_path = f'sqlite:///{parent_path}/database.db'
+
 
 class Product(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True)
-    product_id = Column(String(36), unique=True, nullable=False, default=lambda: str(uuid4()))
-    name = Column(String, nullable=False)
+    id = Column(String(36), primary_key=True, unique=True, nullable=False, default=lambda: str(uuid4()))
+    user_id = Column(String(36), ForeignKey('users.id'), nullable=False)
+    product_name = Column(String, nullable=False)
     price = Column(Integer, nullable=False)
     quantity = Column(Integer, nullable=False)
     description = Column(String)
-    photo = Column(BLOB)
+    photo = Column(String)
     location = Column(String)
     category = Column(String)
     product_rank = Column(Float)
+    
 
-    def __init__(self, product_name, quantity, description, photo, location, category, product_rank, product_id=None):
-        self.name = product_name
+    def __init__(self, product_name, user_id, quantity, description, photo, location, category, product_rank):
+        assert isinstance(product_name, str), "Product name must be a string"
+        assert isinstance(user_id, str), "User ID must be a string"
+        assert isinstance(quantity, int), "Quantity must be an integer"
+        assert isinstance(description, str), "Description must be a string"
+        assert isinstance(photo, str), "Photo must be a BLOB"
+        assert isinstance(location, str), "Location must be a string"
+        assert isinstance(category, str), "Category must be a string"
+        assert isinstance(product_rank, float), "Product rank must be a float"
+        
+        self.product_name = product_name 
+        self.user_id = user_id
         self.price = quantity
         self.quantity = quantity
         self.description = description
         self.photo = photo
-        self.location = location
         self.category = category
-        self.product_rank = product_rank
-        self.product_id = product_id if product_id else str(uuid4())
 
-    def __str__(self):
-        return f"{self.name}, {self.quantity}, {self.description}, {self.photo}, {self.location}, {self.category}, {self.product_rank}, {self.product_id}"
 
-def connect():
-    """ Connect to the SQLite database with an absolute path using SQLAlchemy. """
-    parent_path = Path().resolve()
-    db_path = f'sqlite:///{parent_path}/products.db'
-    engine = create_engine(db_path)
-    Base.metadata.create_all(engine)  # Ensure tables are created
-    return engine
+    def connect(self):
+        """ Connect to the SQLite database with an absolute path using SQLAlchemy. """
+        print(db_path)
+        engine = create_engine(db_path, echo=True)
+        Base.metadata.create_all(engine)  # Ensure tables are created
+        return engine
 
-def save_product(product):
-    """ Insert a new Product into the database using SQLAlchemy session. """
-    engine = connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        session.add(product)
-        session.commit()
-    except SQLAlchemyError as e:
-        print(f"Error: {e}")
-        session.rollback()
-    finally:
-        session.close()
+    def add_product(self):
+        """ Insert a new Product into the database using SQLAlchemy session. """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            session.add(self)
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+            
+    def get_product_by_id(self, product_id):
+        """ Retrieve a Product from the database by its unique identifier. """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            product = session.query(Product).filter_by(product_id=product_id).first()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+        finally:
+            session.close()
+        return product
 
-def main():
-    product = Product("psalidi", 2, "Scissors", "image_data_here", "Warehouse 1", "Tools", 4.8)
-    print('Starting database operations...')
-    save_product(product)  # Save the Product to the database
-    print('Product saved successfully')
+    def get_product_by_user_id(self, user_id):
+        """ Retrieve a Product from the database by its unique identifier. """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            product = session.query(Product).filter_by(user_id=user_id).all()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+        finally:
+            session.close()
+        return product
 
-if __name__ == "__main__":
-    main()
+
+    def update_product(self, product_id, new_product):
+        """ Update the Product in the database from json."""
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            product = session.query(Product).filter_by(product_id=product_id).first()
+            product.product_name = new_product['product_name'] if 'product_name' in new_product and isinstance(new_product['product_name'], str) else product.product_name
+            product.price = new_product['price'] if 'price' in new_product and isinstance(new_product['price'], int) else product.price
+            product.quantity = new_product['quantity'] if 'quantity' in new_product and isinstance(new_product['quantity'], int) else product.quantity
+            product.description = new_product['description'] if 'description' in new_product and isinstance(new_product['description'], str) else product.description
+            product.photo = new_product['photo'] if 'photo' in new_product and isinstance(new_product['photo'], BLOB) else product.photo 
+            product.location = new_product['location'] if 'location' in new_product and isinstance(new_product['location'], str) else product.location
+            product.category = new_product['category'] if 'category' in new_product and isinstance(new_product['category'], str) else product.category
+            product.product_rank = new_product['product_rank'] if 'product_rank' in new_product and isinstance(new_product['product_rank'], float) else product.product_rank
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+        
+        
+
+# def main():
+#     product = Product("psalidi", 2, "Scissors", "image_data_here", "Warehouse 1", "Tools", 4.8)
+#     print('Starting database operations...')
+#     product.add_product(product)  # Save the Product to the database
+#     print('Product saved successfully')
+
+# if __name__ == "__main__":
+#     main()
--- user.py
+++ user.py
@@ -2,9 +2,20 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
+from uuid import uuid4
+import re
 from pathlib import Path
+from product import Product
 
 Base = declarative_base()
+
+parent_path = Path().resolve()
+db_path = f'sqlite:///{parent_path}/database.db'
+
+password_regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+email_regex = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+
+product = Product
 
 class User(Base):
     """
@@ -21,7 +32,7 @@ class User(Base):
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(36), primary_key=True, unique=True, nullable=False, default=lambda: str(uuid4()))
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
@@ -30,6 +41,14 @@ class User(Base):
     ranking = Column(Float, default=4.8)
 
     def __init__(self, name, surname, email, password, location, ranking=4.8):
+        assert isinstance(ranking, float), "Ranking must be a float"
+        assert isinstance(name, str), "Name must be a string"
+        assert isinstance(surname, str), "Surname must be a string"
+        assert email_regex.match(email), "Email must be a valid email address"
+        assert isinstance(password, str), "Password must be a string"
+        assert password_regex.match(password), "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        assert isinstance(location, str), "Location must be a string"
+        
         self.name = name
         self.surname = surname
         self.email = email
@@ -37,59 +56,119 @@ class User(Base):
         self.location = location
         self.ranking = ranking
 
-def connect():
-    """ Connect to the SQLite database with an absolute path using SQLAlchemy. """
-    parent_path = Path().resolve()
-    db_path = f'sqlite:///{parent_path}/users.db'
-    engine = create_engine(db_path)
-    Base.metadata.create_all(engine)  # Ensure tables are created
-    return engine
+    def connect(self):
+        """ Connect to the SQLite database with an absolute path using SQLAlchemy. """
+        engine = create_engine(db_path, echo=True)
+        Base.metadata.create_all(engine)  # Ensure tables are created
+        return engine
 
-def save_user(user):
-    """ Insert a new user into the database using SQLAlchemy session. """
-    engine = connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        session.add(user)
-        session.commit()
-    except SQLAlchemyError as e:
-        print(f"Error: {e}")
-        session.rollback()
-    finally:
-        session.close()
+    def get_user_by_email(self, email):
+        """ Retrieve a user from the database by their email address. """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user = session.query(User).filter_by(email=email).first()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+        finally:
+            session.close()
+        return user
 
-def update_user_email(user, new_email):
-    """ Update the email address of the user in the database. """
-    engine = connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        user.email = new_email
-        session.commit()
-    except SQLAlchemyError as e:
-        print(f"Error: {e}")
-        session.rollback()
-    finally:
-        session.close()
+    def get_user_by_id(self, user_id):
+        """ Retrieve a user from the database by their unique identifier. """
+        
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user = session.query(User).filter_by(user_id=user_id).first()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+        finally:
+            session.close()
+        return user
 
-def update_user_password(user, new_password):
-    """ Update the user's password in the database. """
-    engine = connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        user.password = new_password
-        session.commit()
-    except SQLAlchemyError as e:
-        print(f"Error: {e}")
-        session.rollback()
-    finally:
-        session.close()
+    def add_user(self):
+        """ Insert a new user into the database using SQLAlchemy session. """
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            session.add(self)
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+
+    def update_user_email(self, user, new_email):
+        """ Update the email address of the user in the database. """
+        assert email_regex.match(new_email), "Email must be a valid email address"
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user.email = new_email
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+
+    def update_user_password(self, user, new_password):
+        """ Update the user's password in the database. """
+        assert password_regex.match(new_password), "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user.password = new_password
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+
+
+    def update_user_location(self, user, location):
+        """ Update the location address of the user in the database. """
+        assert isinstance(location, str), "Location must be a string"
+        
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user.location = location
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+            
+    def update_user_ranking(self, user, ranking):
+        """ Update the ranking of the user in the database. """
+        assert isinstance(ranking, float), "Ranking must be a float"
+        engine = self.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        try:
+            user.ranking = ranking
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            session.rollback()
+        finally:
+            session.close()
+
 
 def main():
-    user = User("John", "Doe", "john.doe@example.com", "securepassword123", "New York")
-    save_user(user)  # Save the user to the database
+    user = User("Joh1n", "Doe1", "john.d1oe@example.com", "S!ecurepa1ssword123", "New1York")
+    user.add_user()  # Save the user to the database
     print('User saved successfully')
 
 if __name__ == "__main__":
